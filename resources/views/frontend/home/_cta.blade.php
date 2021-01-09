34651f88c75b356bbc34e3af7f7fbb83117f159a
dung138 <dung13890@gmail.com>
2021-01-09 21:52:28 +0700

    refs #change text

--- resources/views/frontend/home/_cta.blade.php
+++ resources/views/frontend/home/_cta.blade.php
@@ -2,7 +2,7 @@
     <div class="container mx-auto">
         <div class="cta lg:flex lg:justify-between py-8">
             <div class="flex items-center w-full mb-4 lg:mb-0 justify-center lg:justify-normal text-center lg:text-left">
-                <h2 class="text-2xl uppercase text-white">Real estate marketing professionals</h2>
+                <h2 class="text-2xl uppercase text-white">HIGH-QUALITY IMAGE EDITING SERVICE</h2>
             </div>
             <div class="flex items-center w-full justify-center">
                 <a href="{{ route('frontend.page.pricing') }}" class="py-2 px-6 uppercase bg-primary text-white border border-solid border-primary hover:bg-transparent hover:text-primary rounded mx-auto sm:mx-2 block sm:inline-block transition-colors duration-300">Get Pricing</a>
